e2fa2686799d6a51f5a15fa73ab78b11150dff9d
Francis <franciskarani15@gmail.com>
2025-09-02 11:59:04 +0300

    adds the cars for booking list

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import List from '../components/List'
 
 function Book() {
   return (
     <div>
       <Navbar />
+      <List />
       <Footer />
     </div>
   )
