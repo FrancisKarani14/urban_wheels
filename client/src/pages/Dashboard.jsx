cd8d21b0a89eb4e1edd73c1f9fdc6aba82181115
Francis <franciskarani15@gmail.com>
2025-10-10 12:55:21 +0300

    creates an admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default function Dashboard() {
   return (
-    <div>
+      <div>
+          <Navbar />
+          <h1>Admin Dashboard</h1>
+          
+          <Footer/>
       
     </div>
   )
