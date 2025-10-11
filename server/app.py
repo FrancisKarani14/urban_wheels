23206842a197762044cdb001b9774377aef7126b
Francis <franciskarani15@gmail.com>
2025-10-11 14:57:50 +0300

    creates a psql db

--- server/app.py
+++ server/app.py
@@ -1,6 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from models import db, User, Car, Reservation
+
+
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://francis:DevKarani@localhost/urban_wheels_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+CORS(app)
+api = Api(app)
+
 
 @app.route('/')
 def home():
